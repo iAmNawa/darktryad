7d101b74b12e1e4885793cc03fa8d96430d2a330
Paul Borawski <paulborawski14@gmail.com>
2016-04-26 13:27:10 -0700

    website working

---
+++ darktryad.js
@@ -0,0 +1,15 @@
+var port = 666;
+var app = require('express')();
+
+app.use(require('compression')());
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({ extended:true }));
+app.use(require('express').static('public'));
+
+require('http').createServer(app).listen(port,function(){
+  console.log('server is running, on port', port)
+});
+
+
+//app.set is for configurations
+//app.use is for middleware
